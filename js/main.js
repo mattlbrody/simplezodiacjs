3b336558251b7f1efbde0e8407362bfd8b7a0e29
mattlbrody <mattlbrody@gmail.com>
2020-01-29 14:06:47 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,97 @@
+// array of objects to store zodiac data
+var zodiac = [
+	{
+		sign: "aquarius",
+		fortune: "Knowledge, Humanitarian, Serious, Insightful, Duplicitous",
+		image: "img/aquarius.png",
+	},
+	{
+		sign: "aries",
+		fortune: "Active, Demanding, Determined, Effective, Ambitious",
+		image: "img/aries.png"
+	},
+	{
+		sign: "cancer",
+		fortune: "Emotion, Diplomatic, Intensity, Impulsive, Selective",
+		image: "img/cancer.png"
+	},
+	{
+		sign: "capricorn",
+		fortune: "Determination, Dominance, Perservering, Practical, Willful",
+		image: "img/capricorn.png"
+	},
+	{
+		sign: "gemini",
+		fortune: "Communication, Indecision, Inquisitive, Intelligent, Changeable",
+		image: "img/gemini.png"
+	},
+	{
+		sign: "leo",
+		fortune: "Ruling, Warmth, Generosity, Faithful, Initiative",
+		image: "img/leo.png"
+	},
+	{
+		sign: "libra",
+		fortune: "Balance, Justice, Truth, Beauty, Perfection",
+		image: "img/libra.png"
+	},
+	{
+		sign: "pisces",
+		fortune: "Fluctuation, Depth, Imagination, Reactive, Indecisive",
+		image: "img/pisces.png"
+	},
+	{
+		sign: "sagittarius",
+		fortune: "Philosophical, Motion, Experimentation, Optimism",
+		image: "img/sagittarius.png"
+	},
+	{
+		sign: "scorpio",
+		fortune: "Transient, Self-Willed, Purposeful, Unyielding",
+		image: "img/scorpio.png"
+	},
+	{
+		sign: "taurus",
+		fortune: "	Security, Subtle strength, Appreciation, Instruction, Patience",
+		image: "img/taurus.png"
+	},
+	{
+		sign: "virgo",
+		fortune: "Analyzing, Practical, Reflective, Observation, Thoughtful",
+		image: "img/virgo.png"
+	}
+];
+// This is code so the enter key also fires (runs/invokes/instantiates) the function
+var btn = document.getElementById('userdata');
+btn.addEventListener('keypress', function enterKey(e) {
+	if(e.keyCode == 13) {
+		horoscope();
+	};
+}, false);
+
+// function to determine user horoscope, function fires on user clicking button line 30 index.html
+function horoscope() {
+	// store the tag with id="sign" in var userdata
+	var userdata = document.getElementById("userdata");
+
+	// loop through zodiac array one item at a time
+	for(var i = 0; i < zodiac.length; i = i + 1) {
+
+		// if the value the user typed in, changed to all lowercase letters, is equal to one of our signs, then we do something
+		if(userdata.value.toLowerCase() === zodiac[i].sign) {
+			// get element with id="userSign" and change the text to the user input
+			document.getElementById("yourSign").textContent = userdata.value;
+			// get element with id="icon" and change source attribute to current zodiac image path
+			document.getElementById("icon").src = zodiac[i].image;
+			// // get element with id="yourHoroscope" and change the content to this concatenated string
+			document.getElementById("yourHoroscope").textContent = "Your best attributes are: " + zodiac[i].fortune;
+
+			// stop the function because we found a match and added the data to the screen!!!
+			return;
+		};
+		// if no match is found, do opposite of above, mostly clearing content and images
+		document.getElementById("yourSign").textContent = "Not one of the signs. Try again!";
+		document.getElementById("yourHoroscope").textContent = "";
+		document.getElementById("icon").src = "";
+	};
+};
